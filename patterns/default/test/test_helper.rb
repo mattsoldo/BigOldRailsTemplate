f2c2838c946aeaf02e52f807051a27bc69bfa1d5
Mike Gunderloy <MikeG1@larkfarm.com>
2010-01-31 05:12:50 -0600

    Add collection factory methods to test helper

--- patterns/default/test/test_helper.rb
+++ patterns/default/test/test_helper.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base; def send_welcome_email; end; end
 
 class ActiveSupport::TestCase
 #{mock_include}
-  
+
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
 
@@ -31,8 +31,34 @@ class ActiveSupport::TestCase
   #fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # Helper method_missing override to create nicely-named collections of factory objects
+  # For example: generate_factory_collection_for_current_students(15)
+  # or spawn_factory_collection_for_current_students(15) to get a collection of unsaved objects
+  def method_missing(requested_method, *args)
+    if requested_method.to_s =~ /\\Agenerate_factory_collection_for_(\\w+)\\Z/
+      __send__("create_factory_collection", $1, args[0], true)
+    elsif requested_method.to_s =~ /\\Aspawn_factory_collection_for_(\\w+)\\Z/
+        __send__("create_factory_collection", $1, args[0], false)
+    else
+      # Otherwise handle normally #
+      super
+    end
+  end
+
+  # Create a collection of factory objects with option to save or build
+  def create_factory_collection(factory_name, size = 5, save_to_database = false)
+    returning collection = [] do
+      size.times do
+        collection << (save_to_database ? factory_name.classify.constantize.generate() : factory_name.classify.constantize.spawn())
+      end
+    end
+  end
+
 end
 
+# Make authlogic available to all controller tests
 class ActionController::TestCase
   setup :activate_authlogic
 end
+
